1256ed9bb50209abefc7c873b29acfee7dde52e5
Cedric <cedric.balmer@gmx.ch>
2019-11-21 09:35:51 -0800

    finished first exercise

---
+++ Daily-Excercise/JavaScript/Day1.js
@@ -0,0 +1,24 @@
+const d = new Date ();
+const weekDays = ["sunday","monday","tuesday","wednsday",
+"thursday","friday","saturday",];
+const getToday = weekDays[d.getDay()];
+console.log(`Today is: ${getToday}`);
+
+const
+hour = d.getHours(),
+minutes = d.getMinutes()
+seconds = d.getSeconds();
+
+let prepand = (hour < 12) ? 'AM' : 'PM',
+currentTime = () => (`Current time is:  
+${hour} ${prepand} ${minutes} : ${seconds}`)
+console.log(currentTime());
+
+
+// print today's day
+const timeDisplay = document.querySelector('#timeDisplay');
+const dayDisplay = `Today is : ${getToday}
+Current Time is: ${hour} PM : ${minutes} : ${seconds}`;
+
+// document.getElementById('timeDisplay').innerHTML = currentTime();
+timeDisplay.insertAdjacentHTML('afterbegin', dayDisplay);
